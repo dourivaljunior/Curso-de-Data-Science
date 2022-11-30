bba05c3c2e5552ba3a967bc07560c197611bd230
Prof. Dourival Júnior <100372396+dourivaljunior@users.noreply.github.com>
2022-11-30 11:10:02 -0300

    Add files via upload

---
+++ exemplo01.py
@@ -0,0 +1,97 @@
+"""print('curso de python.')
+
+# variáveis
+
+a=input("escreva seu nome ")
+print(a)
+print(type(a))
+
+b=int(input("escreva um número inteiro "))
+print(b)
+print(type(b))
+
+c=float(input("escreva um número real "))
+print(c)
+print(type(c)) """
+
+"""a=10.5
+b=int(a)
+print(b)
+print(type(b))
+
+num=2
+val=str(num)
+print(val)
+print(type(val))
+
+i=3
+r=float(i)
+print(r)
+print(type(r))"""
+
+"""a=11
+b=3
+soma=a+b
+print(soma)
+subtracao=a-b
+print(subtracao)
+multiplicacao=a*b
+print(multiplicacao)
+divisao=a/b
+print(divisao)
+potencia=a**b
+print(potencia)
+divisao_inteira=a//b
+print(divisao_inteira)
+resto_da_divisao=a%b
+print(resto_da_divisao)"""
+
+'''
+#condicional
+nome='python'
+if(nome=='python'):
+    print('linguagem de programação ',nome)
+
+
+'''
+'''a=10
+b=1
+if(a>b):
+    print('a é maior que b')
+elif(a<b):
+    print('b é maior que a')
+else:
+    print('a é igual a b')
+'''
+
+
+"""media=float(input("escreva o valor da nota final do aluno "))
+if(media>=6):
+    print('aprovado')
+else:
+    print('reprovado')
+
+"""
+"""
+media=float(input("escreva o valor da nota final do aluno "))
+if(media>=6 and media<10):
+    print('aprovado')
+if(media<6):
+    print('reprovado')
+if(media==10):
+    print('aprovado com distinção')"""
+
+"""nota1=float(input("escreva o 1a nota do aluno "))
+nota2=float(input("escreva o 2a nota do aluno "))
+nota3=float(input("escreva o 3a nota do aluno "))
+media=(nota1+nota2+nota3)/3
+if(media>=6):
+    print('aprovado com média',media)
+else:
+    print('reprovado com média',media)"""
+
+numero=int(input("escreva um número "))
+if(numero%2==0):
+    print('número par')
+else:
+    print('número impar')
